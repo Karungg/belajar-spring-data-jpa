4abe46f632adc464126b85e538cb06d42779fc5e
Karungg <miftahfadilah71@gmail.com>
2025-06-17 05:55:46 +0700

    CRUD Repository

--- src/main/java/belajarspringdatajpa/belajar_spring_data_jpa/repository/CategoryRepository.java
+++ src/main/java/belajarspringdatajpa/belajar_spring_data_jpa/repository/CategoryRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import belajarspringdatajpa.belajar_spring_data_jpa.entity.Category;
 
 @Repository
-public class CategoryRepository implements JpaRepository<Category, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 }
---
+++ src/test/java/belajarspringdatajpa/belajar_spring_data_jpa/repository/CategoryRepositoryTest.java
@@ -0,0 +1,41 @@
+package belajarspringdatajpa.belajar_spring_data_jpa.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import belajarspringdatajpa.belajar_spring_data_jpa.entity.Category;
+
+@SpringBootTest
+public class CategoryRepositoryTest {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    void testCreate() {
+        Category category = new Category();
+        category.setName("GADGET");
+
+        categoryRepository.save(category);
+
+        assertNotNull(category);
+    }
+
+    @Test
+    void testUpdate() {
+        Category category = categoryRepository.findById(1L).orElse(null);
+
+        category.setName("GADGET MURAH");
+        categoryRepository.save(category);
+
+        category = categoryRepository.findById(1L).orElse(null);
+        assertNotNull(category);
+        assertEquals("GADGET MURAH", category.getName());
+
+    }
+
+}
