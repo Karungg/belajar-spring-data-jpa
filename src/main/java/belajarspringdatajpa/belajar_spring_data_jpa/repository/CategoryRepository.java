53ace443b6cd5c03d7418212ad77e4404d548d97
Karungg <miftahfadilah71@gmail.com>
2025-06-16 18:39:03 +0700

    Repository

---
+++ src/main/java/belajarspringdatajpa/belajar_spring_data_jpa/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package belajarspringdatajpa.belajar_spring_data_jpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import belajarspringdatajpa.belajar_spring_data_jpa.entity.Category;
+
+@Repository
+public class CategoryRepository implements JpaRepository<Category, Long> {
+
+}
