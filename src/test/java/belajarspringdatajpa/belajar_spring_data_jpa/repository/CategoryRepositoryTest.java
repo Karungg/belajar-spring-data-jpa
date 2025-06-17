2724f7bf4d9deededb1c82b5169672d53ec63811
Karungg <miftahfadilah71@gmail.com>
2025-06-17 08:07:53 +0700

    Query method

--- src/test/java/belajarspringdatajpa/belajar_spring_data_jpa/repository/CategoryRepositoryTest.java
+++ src/test/java/belajarspringdatajpa/belajar_spring_data_jpa/repository/CategoryRepositoryTest.java
@@ -3,6 +3,9 @@ package belajarspringdatajpa.belajar_spring_data_jpa.repository;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -36,6 +39,21 @@ public class CategoryRepositoryTest {
         assertNotNull(category);
         assertEquals("GADGET MURAH", category.getName());
 
+    }
+
+    @Test
+    void testFindAllByNameEquals() {
+        Category category = new Category();
+        category.setName("GADGET MURAH");
+        categoryRepository.save(category);
+
+        Category categoryData = categoryRepository.findFirstByNameEquals("GADGET MURAH").orElse(null);
+        assertNotNull(categoryData);
+        assertEquals("GADGET MURAH", categoryData.getName());
+
+        List<Category> categories = categoryRepository.findAllByNameLike("%GADGET%");
+        assertEquals(1, categories.size());
+        assertEquals("GADGET MURAH", categories.get(0).getName());
     }
 
 }
